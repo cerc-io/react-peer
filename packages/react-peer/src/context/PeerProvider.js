d51f9c60f33ea45502549952566750083d82f32b
prathamesh0 <prathamesh.musale0@gmail.com>
2023-02-14 10:21:05 +0530

    Persist peer id to browser local storage

--- packages/react-peer/src/context/PeerProvider.js
+++ packages/react-peer/src/context/PeerProvider.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Peer } from '@cerc-io/peer';
+import { Peer, createPeerId } from '@cerc-io/peer';
 
 import { PeerContext } from './PeerContext';
 
@@ -15,12 +15,26 @@ export const PeerProvider = ({ relayNode, children }) => {
       }
 
       const peer = new Peer(relayNode)
-      await peer.init();
+
+      // Try to get peer id from browser's local storage
+      let peerIdFromStorage = localStorage.getItem('PeerId');
+      let peerIdJson
+
+      if (peerIdFromStorage) {
+        console.log('Using saved peer id; keep the app open in only one browser tab at a time')
+        peerIdJson = JSON.parse(peerIdFromStorage)
+      } else {
+        console.log('Creating a new peer id')
+        peerIdJson = await createPeerId();
+      }
+
+      await peer.init(peerIdJson);
 
       // Debug
       console.log(`Peer ID: ${peer.peerId.toString()}`);
 
       setPeer(peer);
+      localStorage.setItem('PeerId', JSON.stringify(peerIdJson));
     };
 
     init();
