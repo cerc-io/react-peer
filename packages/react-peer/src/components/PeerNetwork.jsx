5308656389e26804464ca03b7d1bb9580e47db52
nabarun <nabarun@deepstacksoft.com>
2023-02-21 18:54:42 +0530

    Use throttling for peer events and break debug components

--- packages/react-peer/src/components/PeerNetwork.jsx
+++ packages/react-peer/src/components/PeerNetwork.jsx
@@ -1,45 +1,45 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useCallback } from 'react';
+import throttle from 'lodash/throttle'
 
 import { Box } from '@mui/material';
 import ScopedCssBaseline from '@mui/material/ScopedCssBaseline';
 
 import { useForceUpdate } from '../hooks/forceUpdate';
 import { PeerContext } from '../context/PeerContext';
-import { DEFAULT_REFRESH_INTERVAL } from '../constants';
+import { DEFAULT_REFRESH_INTERVAL, THROTTLE_WAIT_TIME } from '../constants';
 import NetworkGraph from './NetworkGraph';
 
 export function PeerNetwork ({ refreshInterval = DEFAULT_REFRESH_INTERVAL, ...props }) {
-  const forceUpdate = useForceUpdate();
   const peer = useContext(PeerContext);
+  const forceUpdate = useForceUpdate();
+  const throttledForceUpdate = useCallback(throttle(forceUpdate, THROTTLE_WAIT_TIME), [forceUpdate]);
 
   useEffect(() => {
     if (!peer || !peer.node) {
       return
     }
 
-    peer.node.peerStore.addEventListener('change:multiaddrs', forceUpdate)
-    peer.node.addEventListener('peer:connect', forceUpdate)
-    peer.node.addEventListener('peer:disconnect', forceUpdate)
+    peer.node.addEventListener('peer:connect', throttledForceUpdate)
+    peer.node.addEventListener('peer:disconnect', throttledForceUpdate)
 
     return () => {
-      peer.node?.peerStore.removeEventListener('change:multiaddrs', forceUpdate)
-      peer.node?.removeEventListener('peer:connect', forceUpdate)
-      peer.node?.removeEventListener('peer:disconnect', forceUpdate)
+      peer.node?.removeEventListener('peer:connect', throttledForceUpdate)
+      peer.node?.removeEventListener('peer:disconnect', throttledForceUpdate)
     }
-  }, [peer, forceUpdate])
+  }, [peer, throttledForceUpdate])
 
   useEffect(() => {
     // TODO: Add event for connection close and remove refresh in interval
-    const intervalID = setInterval(forceUpdate, refreshInterval);
+    const intervalID = setInterval(throttledForceUpdate, refreshInterval);
 
     return () => {
       clearInterval(intervalID)
     }
-  }, [forceUpdate])
+  }, [throttledForceUpdate])
 
   return (
     <ScopedCssBaseline>
-      <Box mt={1}>
+      <Box mt={1} {...props}>
         { peer && (
           <NetworkGraph
             peer={peer}
